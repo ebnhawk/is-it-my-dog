27f5c24aff84890ed52e9c8bfc1b48c22b2c1eb3
Molly Hanes <mollyfhanes@gmail.com>
2018-04-28 14:17:31 -0500

    about to change file access to expo

---
+++ components/Home.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Text, View, Image, Button } from 'react-native'
+import styles from '../assets/appStyle'
+
+export default class Home extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+    this.handlePress = this.handlePress.bind(this)
+  }
+
+  handlePress() {
+    return this.props.navigation.navigate('PicSelect')
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.head}>Is It My Dog?</Text>
+        <View style={styles.picContainer}>
+          <Image source={require('../assets/gooby.png')} style={styles.pic} />
+        </View>
+        <Button
+          onPress={this.handlePress}
+          title="TAKE A PICTURE AND FIND OUT"
+          color="#379683"
+        />
+      </View>
+    )
+  }
+}
---
+++ secrets.js
@@ -0,0 +1,1 @@
+export const API = 'd1055eae0fc045269e310f2bc7f71bde'
